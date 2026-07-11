4a447ecd1412aab6c0f3caeec3d1b0806f404946
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-07-11 09:08:16 +0530

    feat: Connect History page to unified backend endpoint and replace audio UI with chat transcript viewer

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,4 +1,7 @@
 const User = require('../models/User');
+const Analysis = require('../models/Analysis');
+const MockInterview = require('../models/MockInterview');
+const Resume = require('../models/Resume');
 
 const getUserProfile = async (req, res) => {
   try {
@@ -20,6 +23,25 @@ const getUserProfile = async (req, res) => {
   }
 };
 
+const getUserHistory = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const analyses = await Analysis.find({ user: userId }).sort({ createdAt: -1 });
+    const sessions = await MockInterview.find({ user: userId }).sort({ createdAt: -1 });
+    const versions = await Resume.find({ user: userId }).sort({ createdAt: -1 });
+
+    res.json({
+      analyses,
+      sessions,
+      versions,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getUserProfile,
+  getUserHistory,
 };
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getUserProfile } = require('../controllers/userController');
+const { getUserProfile, getUserHistory } = require('../controllers/userController');
 const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.get('/profile', protect, getUserProfile);
+router.get('/history', protect, getUserHistory);
 
 module.exports = router;
